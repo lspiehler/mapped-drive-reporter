7d3ee8b5f8337d81f9c4b14bb8cc1f8328915b1d
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2023-02-09 09:09:47 -0600

    add CVDI prefix

--- routes.js
+++ routes.js
@@ -72,6 +72,9 @@ var appRouter = function (app) {
                 } else if(body.COMPUTERNAME.toUpperCase().indexOf("VDI") == 0) {
                     params.sql = "DELETE FROM `drivemappings` WHERE `computername` LIKE ? AND `username` = ?",
                     params.values = ['VDI%', body.USERNAME]
+                } else if(body.COMPUTERNAME.toUpperCase().indexOf("CVDI") == 0) {
+                    params.sql = "DELETE FROM `drivemappings` WHERE `computername` LIKE ? AND `username` = ?",
+                    params.values = ['CVDI%', body.USERNAME]
                 } else {
                     params.sql = "DELETE FROM `drivemappings` WHERE `computername` = ? AND `username` = ?",
                     params.values = [body.COMPUTERNAME, body.USERNAME]
